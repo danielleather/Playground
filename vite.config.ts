54bfaa70404cf74784d5dd2a2e7f6ea01fd8c5af
Dan L <daniel.l@covergenius.com>
2025-04-11 13:51:46 -0400

    adding react-intl and pigment-css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { pigment } from '@pigment-css/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pigment({})],
 })
