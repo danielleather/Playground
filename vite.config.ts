0e0434e59929beb07c1af35e9e237211bc06b7d6
Dan L <daniel.l@covergenius.com>
2025-04-23 15:56:29 -0400

    adding storybook and vite svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { pigment } from '@pigment-css/vite-plugin';
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), pigment({})],
+  plugins: [react(), pigment({}), svgr()],
 })
